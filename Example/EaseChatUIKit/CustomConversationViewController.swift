453639853728a5e6619c6fb87a2ceae98ff591f7
朱继超 <984065974@qq.com>
2024-01-09 13:43:17 +0800

    Create CustomConversationViewController.swift

---
+++ Example/EaseChatUIKit/CustomConversationViewController.swift
@@ -0,0 +1,40 @@
+//
+//  CustomConversationViewController.swift
+//  EaseChatUIKit_Example
+//
+//  Created by 朱继超 on 2024/1/9.
+//  Copyright © 2024 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import EaseChatUIKit
+
+class CustomConversationViewController: ConversationListController {
+    
+    
+    override func createNavigationBar() -> EaseChatNavigationBar {
+        EaseChatNavigationBar()
+    }
+    
+    override func createList() -> ConversationList {
+        ConversationList()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
