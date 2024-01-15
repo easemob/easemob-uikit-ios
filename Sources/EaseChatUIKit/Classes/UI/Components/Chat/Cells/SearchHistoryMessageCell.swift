8e8e1755b6a8ff10d0abf14bf0879ae83132d34c
朱继超 <984065974@qq.com>
2024-01-15 10:53:25 +0800

    search message

---
+++ Sources/EaseChatUIKit/Classes/UI/Components/Chat/Cells/SearchHistoryMessageCell.swift
@@ -0,0 +1,72 @@
+//
+//  SearchHistoryMessageCell.swift
+//  EaseChatUIKit
+//
+//  Created by 朱继超 on 2024/1/15.
+//
+
+import UIKit
+
+@objcMembers open class SearchHistoryMessageCell: UITableViewCell {
+
+    public private(set) lazy var avatar: ImageView = {
+        ImageView(frame: CGRect(x: 16, y: (self.contentView.frame.height-50)/2.0, width: 50, height: 50)).cornerRadius(.large).backgroundColor(.clear)
+    }()
+    
+    public private(set) lazy var conversationName: UILabel = {
+        UILabel(frame: CGRect(x: self.avatar.frame.maxX+12, y: self.avatar.frame.minX+4, width: self.contentView.frame.width-self.avatar.frame.maxX-12-16-50, height: 18)).backgroundColor(.clear).font(UIFont.theme.labelLarge).textColor(UIColor.theme.neutralColor1)
+    }()
+    
+    public private(set) lazy var messageContent: UILabel = {
+        UILabel(frame: CGRect(x: self.avatar.frame.maxX+12, y: self.avatar.frame.maxX-20, width: self.contentView.frame.width-self.avatar.frame.maxX-12-16-50, height: 16)).backgroundColor(.clear)
+    }()
+
+    @objc public required override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.contentView.backgroundColor = .clear
+        self.backgroundColor = .clear
+        self.contentView.addSubViews([self.avatar,self.conversationName,self.messageContent])
+        Theme.registerSwitchThemeViews(view: self)
+        
+    }
+    
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    open override func layoutSubviews() {
+        super.layoutSubviews()
+        self.avatar.center = CGPoint(x: self.avatar.center.x, y: self.contentView.center.y)
+        self.conversationName.frame =  CGRect(x: self.avatar.frame.maxX+12, y: self.avatar.frame.minX+4, width: self.contentView.frame.width-self.avatar.frame.maxX-12-16-50, height: 18)
+        self.messageContent.frame = CGRect(x: self.avatar.frame.maxX+12, y: self.avatar.frame.maxX-20, width: self.contentView.frame.width-self.avatar.frame.maxX-12-16-50, height: 16)
+    }
+    
+    func refresh(message: ChatMessage,info: ConversationInfo,keyword: String) {
+        let nickName = info.nickname.isEmpty ? info.id:info.nickname
+        self.conversationName.text = nickName
+        self.messageContent.attributedText = self.highlightKeywords(keyword: keyword, in: message.showType)
+        self.avatar.image(with: info.avatarURL, placeHolder: info.type == .chat ? Appearance.conversation.singlePlaceHolder:Appearance.conversation.groupPlaceHolder)
+    }
+    
+    func highlightKeywords(keyword: String, in string: String) -> NSAttributedString {
+        let attributedString = NSMutableAttributedString {
+            AttributedText(string).foregroundColor(Theme.style == .dark ? UIColor.theme.neutralColor98:UIColor.theme.neutralColor1)
+        }
+        if !keyword.isEmpty {
+            var range = (string as NSString).range(of: keyword, options: .caseInsensitive)
+            while range.location != NSNotFound {
+                attributedString.addAttribute(.foregroundColor, value: Theme.style == .dark ? UIColor.theme.primaryColor6:UIColor.theme.primaryColor5, range: range)
+                let remainingRange = NSRange(location: range.location + range.length, length: string.count - (range.location + range.length))
+                range = (string as NSString).range(of: keyword, options: .caseInsensitive, range: remainingRange)
+            }
+        }
+        return attributedString
+    }
+}
+
+extension SearchHistoryMessageCell: ThemeSwitchProtocol {
+    public func switchTheme(style: ThemeStyle) {
+        self.conversationName.textColor = style == .dark ? UIColor.theme.neutralColor98:UIColor.theme.neutralColor1    
+    }
+    
+}
---
+++ Sources/EaseChatUIKit/Classes/UI/Components/Chat/Controllers/SearchHistoryMessagesViewController.swift
@@ -0,0 +1,189 @@
+//
+//  SearchHistoryMessagesViewController.swift
+//  EaseChatUIKit
+//
+//  Created by 朱继超 on 2024/1/15.
+//
+
+import UIKit
+
+@objcMembers open class SearchHistoryMessagesViewController: UITableViewController {
+
+    private var searchKeyWord = ""
+    
+    public var rawDatas = [ChatMessage]()
+    
+    private var selectClosure: ((ChatMessage) -> Void)?
+    
+    private var service: ChatService?
+    
+    public private(set) var searchResults = [ChatMessage]()
+    
+    public private(set) lazy var searchController: UISearchController = {
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        searchController.delegate = self
+        searchController.searchBar.showsCancelButton = true
+        searchController.hidesNavigationBarDuringPresentation = true
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.automaticallyShowsSearchResultsController = true
+        searchController.showsSearchResultsController = true
+        searchController.automaticallyShowsScopeBar = false
+        searchController.searchBar.backgroundImage = UIImage()
+        searchController.searchBar.delegate = self
+        return searchController
+    }()
+        
+    public private(set) lazy var empty: EmptyStateView = {
+        EmptyStateView(frame: CGRect(x: 0, y: 0, width: self.tableView.frame.width, height: self.tableView.frame.height),emptyImage: UIImage(named: "empty",in: .chatBundle, with: nil)) {
+            
+        }
+    }()
+    
+    /// ``SearchHistoryMessagesViewController`` init method.Only available in Objective-C language.
+    /// - Parameters:
+    ///   - conversationId: ID of the conversation.
+    ///   - action: Select row callback.
+    @objc public required init(conversationId: String,action: @escaping (ChatMessage) -> Void) {
+        self.service = ChatServiceImplement(to: conversationId)
+        self.selectClosure = action
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    open override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.navigationBar.isHidden = true
+    }
+    
+    open override func viewIsAppearing(_ animated: Bool) {
+        super.viewIsAppearing(animated)
+        self.navigationController?.navigationBar.isHidden = true
+    }
+    
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.tableFooterView(UIView()).rowHeight(Appearance.contact.rowHeight).tableHeaderView(self.searchController.searchBar)
+        self.tableView.keyboardDismissMode = .onDrag
+        Theme.registerSwitchThemeViews(view: self)
+        self.switchTheme(style: Theme.style)
+    }
+    
+    
+
+    // MARK: - Table view data source
+
+    open override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    open override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        if self.searchResults.count <= 0 {
+            self.tableView.backgroundView = self.empty
+        } else {
+            self.tableView.backgroundView = nil
+        }
+        return self.searchResults.count
+    }
+
+    open override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "EaseUIKit_SearchHistoryMessageCell") as? SearchHistoryMessageCell
+        if cell == nil {
+            cell = SearchHistoryMessageCell(style: .default, reuseIdentifier: "EaseUIKit_SearchHistoryMessageCell")
+        }
+        if let item = self.searchResults[safe: indexPath.row] {
+            let conversation = EaseChatUIKitContext.shared?.conversationsCache?[item.conversationId]
+            let info = ConversationInfo()
+            info.id = item.conversationId
+            info.nickname = conversation?.nickname ?? item.conversationId
+            cell?.refresh(message: item,info: info,keyword: self.searchKeyWord)
+        }
+        cell?.selectionStyle = .none
+        cell?.backgroundColor = .clear
+        return cell ?? UITableViewCell()
+    }
+    
+    open override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        if self.searchController.isActive,let item = self.searchResults[safe: indexPath.row] {
+            self.selectClosure?(item)
+        }
+        self.tableView.reloadData()
+    }
+
+}
+
+
+extension SearchHistoryMessagesViewController: UISearchResultsUpdating,UISearchControllerDelegate,UISearchBarDelegate {
+
+    public func updateSearchResults(for searchController: UISearchController) {
+        searchController.searchResultsController?.view.isHidden = false
+        let searchText = searchController.searchBar.text?.lowercased() ?? ""
+        self.searchKeyWord = searchText
+        self.service?.searchMessage(keyword: self.searchKeyWord, pageSize: 1, userId: "", completion: { [weak self] error, messages in
+            if error == nil {
+                self?.rawDatas.removeAll()
+                self?.rawDatas.append(contentsOf: messages)
+                self?.filterResultsWithSearchString(searchText)
+            } else {
+                consoleLogInfo("search message error:\(error?.errorDescription ?? "")", type: .error)
+            }
+        })
+        
+    }
+    
+    @objc open func filterResultsWithSearchString(_ searchText: String) {
+        self.searchResults = self.rawDatas.filter({ message in
+            let showName = message.showType
+            return (showName.lowercased() as NSString).range(of: searchText).location != NSNotFound && (showName.lowercased() as NSString).range(of: searchText).length >= 0
+        })
+        self.tableView.reloadData()
+    }
+    
+    
+    
+    public func willPresentSearchController(_ searchController: UISearchController) {
+        self.searchController = searchController
+    }
+    
+    public func didPresentSearchController(_ searchController: UISearchController) {
+        
+    }
+    
+    public func willDismissSearchController(_ searchController: UISearchController) {
+        
+    }
+    
+    public func didDismissSearchController(_ searchController: UISearchController) {
+        
+    }
+    
+    public func presentSearchController(_ searchController: UISearchController) {
+        
+    }
+    
+    public func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        if self.navigationController != nil {
+            self.navigationController?.popViewController(animated: true)
+        } else {
+            self.dismiss(animated: true)
+        }
+    }
+}
+
+extension SearchHistoryMessagesViewController: ThemeSwitchProtocol {
+    open func switchTheme(style: ThemeStyle) {
+        self.searchController.searchBar.backgroundColor(style == .dark ? UIColor.theme.neutralColor1:UIColor.theme.neutralColor98)
+        self.searchController.searchBar.barStyle = style == .dark ? .black:.default
+        self.tableView.backgroundColor(style == .dark ? UIColor.theme.neutralColor1:UIColor.theme.neutralColor98)
+        self.tableView.reloadData()
+    }
+    
+    
+}
