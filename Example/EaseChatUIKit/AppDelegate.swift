bb6e5e22a6e236dc0d33204721a56092b10c9b96
朱继超 <984065974@qq.com>
2024-11-28 17:02:25 +0800

    appkey

--- Example/EaseChatUIKit/AppDelegate.swift
+++ Example/EaseChatUIKit/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        let option = ChatOptions(appkey: "easemob#easeim")
+        let option = ChatOptions(appkey: <#Your App Key#>)
         option.enableConsoleLog = true
         _ = EaseChatUIKitClient.shared.setup(option: option)
         let redPackage = ActionSheetItem(title: "红包".chat.localize, type: .normal,tag: "Red",image: UIImage(named: "photo", in: .chatBundle, with: nil))
